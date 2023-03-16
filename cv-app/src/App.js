770949cc58717858715be0929f889387b4b04a7f
pritalb <pritalbamnodkar@gmail.com>
2023-03-16 15:53:03 +0530

    added edit form to IntroSection component and deleted ResumeForm.js and ResumeForm.css

--- cv-app/src/App.js
+++ cv-app/src/App.js
@@ -1,11 +1,9 @@
 import './styles/App.css';
 import Resume from './components/Resume';
-import ResumeForm from './components/ResumeForm';
 
 function App() {
   return (
     <div className="app">
-        <ResumeForm />
         <Resume />
     </div>
   );
--- cv-app/src/components/IntroSection.js
+++ cv-app/src/components/IntroSection.js
@@ -1,7 +1,51 @@
 import React, { Component } from 'react';
 import '../styles/IntroSection.css';
 
-class IntroSection extends Component {
+class IntroForm extends Component {
+    constructor() {
+        super()
+    };
+
+    render() {
+        return (
+
+            <div>
+                <div>
+                    <label htmlFor='name-info'> Enter your Name: </label>
+                    <input name='name-info'></input>
+                </div>
+
+                <div>
+                    <label htmlFor='jobrole-info'> Enter your Job Role: </label>
+                    <input name='jobrole-info'></input>
+                </div>
+
+                <div>
+                    <label htmlFor='jobrole-info'> Enter your Profile Picture URL: </label>
+                    <input type="url" name='profile-picture-url'></input>
+                </div>
+
+                <div>
+                    <label htmlFor='contact-number-info'> Enter your Contact Number: </label>
+                    <input type="number" name='contact-number-info'></input>
+                </div>
+
+                <div>
+                    <label htmlFor='email-info'> Enter your Email: </label>
+                    <input type="email" name='email-info'></input>
+                </div>
+
+                <div>
+                    <label htmlFor='profile-summary'> Enter your Profile Summary: </label>
+                    <textarea name='profile-summary'></textarea>
+                </div>
+            </div>
+        );
+    }
+
+}
+
+class IntroInfo extends Component {
     constructor() {
         super();
     }
@@ -18,6 +62,25 @@ class IntroSection extends Component {
                     {/* </div> */}
                 </div>
                 <div id='intro-image-container' > <img id='intro-image' src='https://images.unsplash.com/photo-1568316674077-d72ee56de61c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cHJvZmVzc2lvbmFsJTIwbWFufGVufDB8fDB8fA%3D%3D&w=1000&q=80' /> </div>
+            </div>
+        );
+    }
+}
+
+class IntroSection extends Component {
+    constructor() {
+        super();
+    }
+
+    ShowEditForm() {
+        
+    }
+
+    render() {
+        return (
+            <div>
+                <IntroInfo />
+                {false && <IntroForm />}
             </div>
         );
     }
