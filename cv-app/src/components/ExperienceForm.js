1975e29043c0703774aa17b30315d737803da6f2
pritalb <pritalbamnodkar@gmail.com>
2023-03-21 15:11:09 +0530

    created work experience form component in ExperienceForm

--- cv-app/src/components/ExperienceForm.js
+++ cv-app/src/components/ExperienceForm.js
@@ -105,11 +105,95 @@ class AddEducationForm extends Component {
 class AddWorkExperienceForm extends Component {
     constructor(props) {
         super(props);
+
+        this._AddWorkExperience = this._AddWorkExperience.bind(this);
+        this.SetJobrole = this.SetJobrole.bind(this);
+        this.SetCompany = this.SetCompany.bind(this);
+        this.SetStartDate = this.SetStartDate.bind(this);
+        this.SetEndDate = this.SetEndDate.bind(this);
+        this.SetSummary = this.SetSummary.bind(this);
+
+        this.state = {
+            jobrole: 'Sample Engineer',
+            company: 'Some Company',
+            startDate: '2014',
+            endDate: '2015',
+            summary: 'This is a sample work information. Take it with a grain of salt',
+        };
+    }
+
+    _AddWorkExperience() {
+
+    }
+
+    SetJobrole(e) {
+        const val = e.target.value;
+
+        this.setState({
+            jobrole: val,
+        });
+    }
+
+    SetCompany(e) {
+        const val = e.target.value;
+
+        this.setState({
+            company: val,
+        });
+    }
+
+    SetStartDate(e) {
+        const val = e.target.value;
+
+        this.setState({
+            startDate: val,
+        });
+    }
+
+    SetEndDate(e) {
+        const val = e.target.value;
+
+        this.setState({
+            endDate: val,
+        });
+    }
+
+    SetSummary(e) {
+        const val = e.target.value;
+
+        this.setState({
+            summary: val,
+        });
     }
 
     render() {
         return (
-            <div></div>
+            <div>
+                <div>
+                    <label> Job Role: </label>
+                    <input value={this.state.jobrole} onChange={(event) => this.SetJobrole(event)}/>
+                </div>
+
+                <div>
+                    <label> Company: </label>
+                    <input value={this.state.company} onChange={(event) => this.SetCompany(event)}/>
+                </div>
+
+                <div>
+                    <label> Start Date: </label>
+                    <input type="date" value={this.state.startDate} onChange={(event) => this.SetStartDate(event)}/>
+                </div>
+
+                <div>
+                    <label> End Date: </label>
+                    <input type="date" value={this.state.endDate} onChange={(event) => this.SetEndDate(event)}/>
+                </div>
+                <div>
+                    <label> Summary: </label>
+                    <textarea value={this.state.summary} onChange={(event) => this.SetSummary(event)}></textarea>
+                </div>
+                <button onClick={this._AddWorkExperience}> Add </button>
+            </div>
         );
     }
 }
--- cv-app/src/components/ProfessionalWorkExperience.js
+++ cv-app/src/components/ProfessionalWorkExperience.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import '../styles/ProfessionalWorkExperience.css';
 
+
+
 class WorkExperience extends Component {
     constructor() {
         super();
